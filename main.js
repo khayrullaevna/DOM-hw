ef453527ee4ecfb2eda5652d089bb8a267d84b16
khayrullaevna <khayrullaevna.g@gmail.com>
2023-08-01 13:19:13 +0500

    change

---
+++ main.js
@@ -0,0 +1,18 @@
+const elInput = document.querySelector("#distance");
+const elButton = document.querySelector(".submit");
+
+// const elButton1 = document.querySelector(".piyoda");
+// const elButton2 = document.querySelector(".velosiped");
+// const elButton3 = document.querySelector(".mashina");
+// const elButton4 = document.querySelector(".samolyot");
+
+const elTitle = document.querySelector(".title");
+
+elButton.addEventListener("click", function(evt){
+    evt.preventDefault()
+    elTitle.innerHTML = `Piyoda ${elInput.value*3.6}km,
+    Velosiped ${elInput.value*20.1}km,
+    Mashina ${elInput.value*70}km,
+    Samolyot ${elInput.value*800}km`
+}
+)
